b0db2fe405ffb466b115147ebce2257bf845c8c1
pedroluccs <130405548+pedroluccs@users.noreply.github.com>
2024-10-01 15:44:46 -0400

    conversao da estilizacao com styled components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { HeroForm, HeroDiv, HeroTitle } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <HeroForm>
+    <HeroDiv className="container">
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </HeroTitle>
+    </HeroDiv>
+  </HeroForm>
 )
 
 export default Hero
